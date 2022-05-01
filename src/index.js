eba05db29c8caabd7995e20e6924aee4ba697098
lalomx <lalo0892@gmail.com>
2022-05-01 14:30:53 -0500

    static

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const path = require('path')
+
 const app = express()
 const PORT = process.env.PORT || 8888
 
-app.get('', (req, res) => res.send('ok'))
+app.use('/static', express.static(path.join(__dirname, '../public')))
+app.get('', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')))
 
 app.listen(PORT, () => console.log(`server at http://localhost:${PORT}`))
