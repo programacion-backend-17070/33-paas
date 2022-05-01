245e213e7562f7bc20f919ce3b9a5db7a224dd07
lalomx <lalo0892@gmail.com>
2022-05-01 14:15:33 -0500

    #1 feature

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 8888
 
-
+app.get('', (req, res) => res.send('ok'))
 
 app.listen(PORT, () => console.log(`server at http://localhost:${PORT}`))
