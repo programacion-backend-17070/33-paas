8bb49066eeb90ecfd85b8d352bc509743d9dd1f5
Luciano Schmidt <luciano43schmidt@gmail.com>
2022-05-02 10:25:13 -0300

    #1 crear endpoint de /info

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 const express = require('express')
+const CPUs = require("os").cpus().length
 const app = express()
-const PORT = process.env.PORT || 8888
+const PORT = process.env.PORT || 8082
 
 app.get('', (req, res) => res.send('ok'))
+
+app.get("/info", (req, res) => {
+    res.send({
+        num: CPUs
+    })
+})
 
 app.listen(PORT, () => console.log(`server at http://localhost:${PORT}`))
