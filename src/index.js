4f42e7e2f170553baeb6ad6934d8788681bc935e
lalomx <lalo0892@gmail.com>
2022-05-02 09:27:47 -0500

    agregar archivos estaticos

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const path = require('path')
 const CPUs = require('os').cpus().length
 const app = express()
 const PORT = process.env.PORT || 8888
 
-app.get('', (req, res) => res.send('ok'))
+
+app.use('/static', express.static(path.join(__dirname, '../public')))
+app.get('', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')))
 
 app.get('/info', (req, res) => {
   res.send({
